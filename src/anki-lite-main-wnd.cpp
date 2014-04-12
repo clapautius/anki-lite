98b693d3f27ce6cf7e9f53fba6931b521a64b359
Tudor M. Pristavu <clapautius@gmail.com>
2014-04-12 13:58:49 +0300

    WIP: working on GUI.

--- src/anki-lite-main-wnd.cpp
+++ src/anki-lite-main-wnd.cpp
@@ -1,3 +1,7 @@
+#include <stdexcept>
+#include <QMessageBox>
+#include <QPushButton>
+
 #include "anki-lite-main-wnd.h"
 #include "main.h"
 
@@ -6,12 +10,20 @@ using anki_lite::Collection;
 AnkiLiteMainWnd::AnkiLiteMainWnd( QWidget *, char *)
 {
     setupUi(this);
+
+    // connect close button
+    const QPushButton *p_close_button = mp_button_box->button(QDialogButtonBox::Close);
+    if (p_close_button == NULL) {
+        QMessageBox::critical(NULL, "Error", tr("No close button present. BUG"));
+        throw std::runtime_error("No close button present. BUG");
+    }
+    connect(p_close_button, SIGNAL(clicked(bool)), this, SLOT(close()));
 }
 
 
 void AnkiLiteMainWnd::show_decks(const anki_lite::Collection &collection)
 {
-    // :fixme: clear list first
+    listWidget->clear();
     for (unsigned i = 0; i < collection.get_no_of_decks(); ++i) {
         listWidget->addItem(collection.get_deck(i).name());
     }
